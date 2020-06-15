6a0ccfd1ab8524169fc2aca612ff42f234cdae4f
Danila Polevshchikov <dpolevshchikov@ozon.ru>
2020-06-16 01:58:48 +0300

    azure-функция для анонимного slack-бота

---
+++ AnonSlackBotFunctions/AnonSlackBotCommandProcessor.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AnonSlackBotFunctions
+{
+    public static class AnonSlackBotCommandProcessor
+    {
+        private const string SlackBotWebhookUrl = "https://hooks.slack.com/services/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";
+		
+		/// <summary>
+		/// принимаем запрос от бота в формате https://api.slack.com/interactivity/slash-commands#app_command_handling,
+        /// парсим и вытаскиваем оттуда text. Далее просто кидаем его на webhook бота, привязанного к конкретному каналу )
+        ///
+        /// curl --location --request POST 'localhost:7071/api/AnonSlackBotCommandProcessor' \
+        /// --header 'Content-Type: application/x-www-form-urlencoded' \
+        /// --data-urlencode 'text=sampletext'
+		/// </summary>
+		[FunctionName("AnonSlackBotCommandProcessor")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+        {
+	    try
+	    {
+	        log.LogInformation("Начали парсинг");
+	        var messageText = req.Form["text"];
+	        log.LogInformation($"text from form is {messageText}");
+	        if (!string.IsNullOrEmpty(messageText))
+            {
+                using var client = new HttpClient();
+                //json в формате, понимаемом slack
+                var slackMessage = new StringContent($"{{\"text\" : \"{messageText}\"}}");
+                var result = await client.PostAsync(SlackBotWebhookUrl, slackMessage);
+                log.LogInformation($"webhook call result is {result})");
+                result.EnsureSuccessStatusCode();
+                log.LogInformation("отправлено успешно.");
+                return new OkObjectResult("Сообщение отправлено в канал");
+            }
+	    }
+	    catch (Exception ex)
+	    {
+	        log.LogCritical(ex, "полный провал");
+	    }
+	
+	    log.LogInformation("ошибка");
+	    return new OkObjectResult("Кажется, произошла ошибка");
+        }
+    }
+}
+
+
+
