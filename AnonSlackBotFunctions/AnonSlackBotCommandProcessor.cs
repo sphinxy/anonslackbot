b7b69a6b0bc5332025723940905994d0aee0d9fc
Danila Polevshchikov <dpolevshchikov@ozon.ru>
2020-06-16 02:24:09 +0300

    Сделали webhook postfix и теперь токен в коде не хранится. Доку дописали

--- AnonSlackBotFunctions/AnonSlackBotCommandProcessor.cs
+++ AnonSlackBotFunctions/AnonSlackBotCommandProcessor.cs
@@ -11,7 +11,7 @@ namespace AnonSlackBotFunctions
 {
     public static class AnonSlackBotCommandProcessor
     {
-        private const string SlackBotWebhookUrl = "https://hooks.slack.com/services/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";
+        private const string SlackBotWebhookUrlPrefix = "https://hooks.slack.com/services/";
         
         /// <summary>
         /// принимаем запрос от бота в формате https://api.slack.com/interactivity/slash-commands#app_command_handling,
@@ -25,21 +25,27 @@ namespace AnonSlackBotFunctions
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
         {
+            
             log.LogInformation("Начали парсинг");
             try
             {
+                var webhookPostfix = req.Query["webhookPostfix"];
+                if (string.IsNullOrEmpty(webhookPostfix))
+                {
+                    return new OkObjectResult($"Ошибка. Не передан query-параметр webhookPostfix для {SlackBotWebhookUrlPrefix}[...]!");
+                }
                 var messageText = req.Form["text"];
                 log.LogInformation($"text from form is {messageText}");
-                if (!string.IsNullOrEmpty(messageText))
+                if (!string.IsNullOrEmpty(messageText) )
                 {
                     using var client = new HttpClient();
+                    client.BaseAddress = new Uri(SlackBotWebhookUrlPrefix);
                     //json в формате, понимаемом slack
                     var slackMessage = new StringContent($"{{\"text\" : \"{messageText}\"}}");
-                    var result = await client.PostAsync(SlackBotWebhookUrl, slackMessage);
-                    log.LogInformation($"webhook call result is {result})");
+                    var result = await client.PostAsync(webhookPostfix, slackMessage);
                     result.EnsureSuccessStatusCode();
                     log.LogInformation("отправлено успешно.");
-                    return new OkObjectResult("Сообщение анонимно отправлено в канал");
+                    return new OkResult();
                 }
             }
             catch (Exception ex)
